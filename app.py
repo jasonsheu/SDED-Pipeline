e5195de20f726b64bfc12299d1de39181fe6f0f0
Nicolas de la Fuente <ndelafuente@sandiego.edu>
2021-08-11 22:35:08 -0700

    Developed templates into simple skeleton

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TODO: What should this be?'
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/new-job/')
+@app.route('/new-job/', methods=('GET', 'POST'))
 def create():
-	return render_template('new-job.html')
+	if request.method == 'POST':
+		title = request.form['title']
+		content = request.form['content']
+
+		if not title:
+			flash('Title is required!')
+		else:
+			return redirect(url_for('index'))
+
+	return render_template('create.html')
+
