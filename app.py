23b8192996e4e6a2d35fe06136c12010391677f2
Jason Sheu <jasonsheu27@gmail.com>
2021-08-19 14:47:26 -0700

    added file upload placeholder, updated links

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from ragic_tools import *
+import os
+from werkzeug.utils import secure_filename
+
+
+upload_folder = "/uploads"
+allowed_extensions = {'csv','txt'}
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'TODO: What should this be?'
-
+app.config['UPLOAD_FOLDER'] = upload_folder
 LOG_FILE = "test.json"
+
 
 @app.route('/')
 def index():
@@ -75,3 +83,28 @@ def history():
 @app.route('/favicon.ico')
 def icon():
     return redirect(url_for('static', 'sded-logo.png'))
+
+
+#file uploading
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return ''
